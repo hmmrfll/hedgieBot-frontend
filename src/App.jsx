044e397f5b9d92d7142d1ae8ab44e5a7b756839b
hmmrfll <a.kazakevych@gmail.com>
2024-07-14 20:19:04 +0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useEffect, useState } from 'react'
 import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import './App.css'
