cd48ee43f5f987a4d4a47de12cb758b8606e3ccb
hmmrfll <a.kazakevych@gmail.com>
2024-07-14 11:40:37 +0300

    init

--- src/CreateTracking.jsx
+++ src/CreateTracking.jsx
@@ -1,4 +1,5 @@
 import axios from 'axios'
+// eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import './CreateTracking.css'
