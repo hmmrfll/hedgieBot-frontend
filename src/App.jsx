8c7973ad8dd89d34ecfa980e64160214abf1b634
hmmrfll <a.kazakevych@gmail.com>
2024-07-14 09:29:59 +0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,18 @@ import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import './App.css'
 import CreateTracking from './CreateTracking.jsx'
 
-function App() {
-	console.log('App component rendered')
-
+function Home() {
 	const [options, setOptions] = useState([])
-	const telegramId = window.Telegram.WebApp.initDataUnsafe.user.id
+	const telegramId = window.Telegram?.WebApp?.initDataUnsafe?.user?.id
 
 	useEffect(() => {
 		const fetchOptions = async () => {
+			if (!telegramId) {
+				console.error('Telegram ID not found')
+				return
+			}
 			try {
-				const response = await axios.get(
-					`https://f3d5-2a02-bf0-1413-2ebc-ed86-9e39-25f4-572a.ngrok-free.app/api/tracking/${telegramId}/tracks`
-				)
+				const response = await axios.get(`/api/tracking/${telegramId}/tracks`)
 				setOptions(response.data)
 			} catch (err) {
 				console.error('Error fetching tracks:', err)
@@ -26,25 +26,30 @@ function App() {
 	}, [telegramId])
 
 	return (
+		<div className='home-content'>
+			<h2>Tracked options:</h2>
+			<hr className='separator' />
+			<ul className='options-list'>
+				{options.map((option, index) => (
+					<li key={index}>{option}</li>
+				))}
+			</ul>
+			<Link to='/create-tracking' className='link-button'>
+				<button className='create-tracking-button'>Create Tracking</button>
+			</Link>
+		</div>
+	)
+}
+
+function App() {
+	console.log('App component rendered')
+
+	return (
 		<Router>
 			<div className='app-container'>
 				<Switch>
 					<Route exact path='/'>
-						<div className='home-content'>
-							{console.log('Rendering Home Page')}
-							<h2>Tracked options:</h2>
-							<hr className='separator' />
-							<ul className='options-list'>
-								{options.map((option, index) => (
-									<li key={index}>{option}</li>
-								))}
-							</ul>
-							<Link to='/create-tracking' className='link-button'>
-								<button className='create-tracking-button'>
-									Create Tracking
-								</button>
-							</Link>
-						</div>
+						<Home />
 					</Route>
 					<Route path='/create-tracking'>
 						<CreateTracking />
